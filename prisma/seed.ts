65f0a892ee0e0350655af61c8a19ed7a73a01deb
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-06-26 11:02:14 +0600

    seed script and file added

---
+++ prisma/seed.ts
@@ -0,0 +1,50 @@
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "../generated/prisma/client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaBetterSqlite3({ url: connectionString });
+
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  await prisma.preOrder.createMany({
+    data: [
+      {
+        name: "iPhone 17 Pro Max",
+        products: 150,
+        preOrderWhen: "REGARDLESS_OF_STOCK",
+        status: "ACTIVE",
+        startsAt: new Date("2026-06-01"),
+        endsAt: new Date("2026-07-01"),
+      },
+      {
+        name: "Samsung Galaxy S30",
+        products: 80,
+        preOrderWhen: "OUT_OF_STOCK",
+        status: "ACTIVE",
+        startsAt: new Date("2026-06-10"),
+        endsAt: new Date("2026-07-10"),
+      },
+      {
+        name: "MacBook Pro M7",
+        products: 40,
+        preOrderWhen: "REGARDLESS_OF_STOCK",
+        status: "INACTIVE",
+        startsAt: new Date("2026-06-15"),
+        endsAt: null,
+      },
+    ],
+  });
+
+  console.log(" Sample data seeded successfully");
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
