7f9ad4c04c09879620fe44b30d0cb2d0e361b86a
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-06-26 11:13:01 +0600

    seed file issue is fixed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,9 @@
+import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
-
+console.log(connectionString,'this is stirng')
 const adapter = new PrismaBetterSqlite3({ url: connectionString });
 
 const prisma = new PrismaClient({ adapter });
